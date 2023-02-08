c55c656824ed82d8789993621da30560337d453f
S3Prototype <liuqahs15@gmail.com>
2023-02-08 11:15:00 -0600

    Unnamed function

--- api/handler.js
+++ api/handler.js
@@ -2,13 +2,13 @@
 //   runtime: "edge",
 // };
 
-export default function handler(request, response) {
+export default (request, response) => {
   response.status(200).json({
     body: request.body,
     query: request.query,
     cookies: request.cookies,
   });
-}
+};
 
 // export default (req) => {
 //   return new Response(`Hello, from ${req.url} I'm now an Serverless Function!`);
