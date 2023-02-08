fc30849478761b565be65b751f460481a55cfd1c
S3Prototype <liuqahs15@gmail.com>
2023-02-08 11:08:49 -0600

    Remove edge config

--- api/handler.js
+++ api/handler.js
@@ -1,6 +1,6 @@
-export const config = {
-  runtime: "edge",
-};
+// export const config = {
+//   runtime: "edge",
+// };
 
 export default (req) => {
   return new Response(`Hello, from ${req.url} I'm now an Edge Function!`);
